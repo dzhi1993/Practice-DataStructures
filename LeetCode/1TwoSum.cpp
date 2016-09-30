66c8f8b5a934143cbd6fc32d7a844404182f34c8
Da Zhi <r930710@gmail.com>
2016-09-30 10:28:17 -0400

    Problem 1.Two Sum

--- LeetCode/1TwoSum.cpp
+++ LeetCode/1TwoSum.cpp
@@ -4,21 +4,19 @@
 
 using namespace std;
 
-class Solution{
-	
+class Solution{	
 public:
 	vector<int> twoSum(vector<int> &nums, int target)
 	{
 		vector<int> index;
 		map<int, vector<int>::size_type> indexMap;
-	
+		
 		for (vector<int>::size_type i = 0; i < nums.size(); i++)
 		{
 			if (indexMap.find(nums[i]) == indexMap.end())
 			{
 				indexMap.insert(std::pair<int, vector<int>::size_type>(nums[i], i));
 			}
-	
 			if ((indexMap.find(target - nums[i]) != indexMap.end()) && (indexMap.find(target - nums[i])->second != i))
 			{
 				index.push_back(indexMap.find(target - nums[i])->second);
@@ -27,19 +25,22 @@ public:
 			}
 		}
 	}
-
 };
 
-class anotherSolution {
+class anotherSolution{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int>& nums, int target) 
+	{
         map <int,int> myMap;
         map<int,int>::iterator ite;
         vector<int> ans;
         myMap.insert(pair<int,int>(target - nums[0],0));
-        for (int i = 1;i<nums.size();i++){
+        
+        for (int i = 1;i<nums.size();i++)
+		{
             ite = myMap.find(nums[i]);
-            if(ite!=myMap.end()){
+            if(ite!=myMap.end())
+			{
                 ans.push_back(ite->second);
                 ans.push_back(i);
                 return ans;

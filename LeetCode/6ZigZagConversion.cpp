f12ae5e018dea05e0be0f110d06e3d60e2142877
Da Zhi <da.zhi@microsemi.net>
2016-10-07 10:06:17 -0400

    The complexity is O(s.length() * numRows), which can be decreased.

--- LeetCode/6ZigZagConversion.cpp
+++ LeetCode/6ZigZagConversion.cpp
@@ -7,26 +7,29 @@ class Solution {
 public:
     string convert(string s, int numRows) {
         string substrings[numRows];
-
-        for (int i = 0; i < s.length(); i++) {
-
-            int curr = numRows % ( (numRows - 1) * 2 );
-
-            for (int j = 0; j < numRows; j++) {
-                if ( curr == 0 ) {
-                    substrings[j] += s.at(i);
-                }
-                else if ()
+        int i = 0;
+        while( i < s.length() ) {
+            for (int j = 0; i < s.length() && j < numRows; j ++){
+                substrings[j] += s[i];
+                i++;
             }
-
-
+            for (int j = numRows - 2; i < s.length() && j > 0; j --){
+                substrings[j] += s[i];
+                i++;
+            }
         }
+        string res = "";
+        for (int i = 0; i < numRows; i++){
+            res += substrings[i];
+        }
+        return res;
     }
 };
 
-
 int main()
 {
-    cout << "Hello world!" << endl;
+    aSolution A;
+    string str = A.convert("ABCDEFGHIJKLMNO", 4);
+    cout << str << endl;
     return 0;
 }

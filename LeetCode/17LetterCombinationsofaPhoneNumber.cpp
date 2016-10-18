e7333de6f7835db70f132dfa9523484df01374bd
Da Zhi <r930710@gmail.com>
2016-10-18 13:36:15 -0400

    The complexity is O(n^2)

--- LeetCode/17LetterCombinationsofaPhoneNumber.cpp
+++ LeetCode/17LetterCombinationsofaPhoneNumber.cpp
@@ -9,37 +9,39 @@ public:
     vector<string> letterCombinations(string digits) {
         vector<string> res;
         if (digits.empty()) { return res;}
-        res,push_back("");
+        res.push_back("");
 
         string myarray[] = {"", "", "abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         vector<string> LetterNumber ( myarray, myarray + sizeof(myarray) / sizeof(string) );
-//        LetterNumber[2] = "abc";
-//        LetterNumber[3] = "def";
-//        LetterNumber[4] = "ghi";
-//        LetterNumber[5] = "jkl";
-//        LetterNumber[6] = "mno";
-//        LetterNumber[7] = "pqrs";
-//        LetterNumber[8] = "tuv";
-//        LetterNumber[9] = "wxyz";
+
         for (vector<int>::size_type i = 0; i < digits.size(); i++) {
             int number = digits.at(i) - '0';
+            //cout << number << " ";
+            //cout << res.size();
             //string letter = LetterNumber.find(number)->second;
             if (LetterNumber.at(number) == "") {
+                res.clear();
                 break;
             }
+            vector<string> tmp;
             for (vector<int>::size_type j = 0; j < res.size(); j++) {
                 for (int k = 0; k < LetterNumber.at(number).size(); k++) {
-                    res.push_back( res.at(j) + LetterNumber.at(number).at(k) );
+                    tmp.push_back( res[j] + LetterNumber[number][k] );
                 }
             }
-
+            swap(tmp, res);
         }
         return res;
     }
 };
 
-
 int main() {
+    Solution A;
+    vector<string> a = A.letterCombinations("234645");
+    //cout << a.size();
+    for (int i =0; i < a.size(); i++) {
+        cout << a.at(i) << " ";
+    }
 
-
+    return 0;
 }

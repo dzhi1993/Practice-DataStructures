06dcd85e0db3d0df59088bcc17be9d99424cfa3f
Da Zhi <r930710@gmail.com>
2016-12-01 09:40:20 -0500

    Slightly change: add general idea as a comment.

--- LeetCode/026RemoveDuplicatesFromSortedArray.cpp
+++ LeetCode/026RemoveDuplicatesFromSortedArray.cpp
@@ -4,22 +4,22 @@
 
 using namespace std;
 
+//General idea: In this problem, the input array is sorted.
+//so we just update the next number once a time when its different between previous one
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         if (nums.size() == 0) return 0;
         int i = 1;
-        int  len = 0;
-        while (i < nums.size())
-        {
+        int len = 0;
+        for (int i = 1; i < nums.size(); i++) {
             if (nums[len] != nums[i])
             {
                 nums[len + 1] = nums[i];
                 len++;
             }
-            i++;
         }
-
         return len + 1;
     }
 };
